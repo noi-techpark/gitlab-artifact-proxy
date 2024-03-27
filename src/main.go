868a1320ab79287b0ee90a1160e96a37a501d41d
Simon Dalvai <s.dalvai@noi.bz.it>
2024-03-27 16:52:51 +0100

    serve both json and file

--- src/main.go
+++ src/main.go
@@ -30,14 +30,50 @@ func main() {
 
 	r.Use(gin.Recovery())
 
-	r.GET("/", artifact)
+	r.GET("/", artifactFile)
+	r.GET("/json", artifactJson)
 	r.GET("/health", health)
 	r.Run()
 }
 func health(c *gin.Context) {
 	c.Status(http.StatusOK)
 }
-func artifact(c *gin.Context) {
+func artifactJson(c *gin.Context) {
+	req, err := http.NewRequest(http.MethodGet, "https://gitlab.com/api/v4/projects/54944672/jobs/artifacts/main/raw/report.json?job=harvest", nil)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
+
+	req.Header = http.Header{
+		"PRIVATE-TOKEN": {token},
+		"Accept":        {"application/json"},
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
+
+	var data map[string]interface{}
+	json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
+
+	c.JSON(http.StatusOK, data)
+}
+
+func artifactFile(c *gin.Context) {
 	req, err := http.NewRequest(http.MethodGet, "https://gitlab.com/api/v4/projects/54944672/jobs/artifacts/main/raw/report.json?job=harvest", nil)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
